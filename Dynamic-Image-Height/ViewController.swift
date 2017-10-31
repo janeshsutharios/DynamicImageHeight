5e31095d65165a23e875763758ffe92450a64dff
AdityaBirlaHealth Common <abhcommon@AdityaBirlaHealths-Mac-mini.local>
2017-10-31 11:39:39 +0530

    Initial Commit

---
+++ Dynamic-Image-Height/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dynamic-Image-Height
+//
+//  Created by AdityaBirlaHealth Common on 31/10/17.
+//  Copyright © 2017 Janesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
